af2029b80871bd9dede2e0efaf3241e99c7a7fe3
nmhummel <natalie.hummel@gmail.com>
2021-01-05 11:47:08 -0600

    commit tree eae31f564febee670aa55353cdc87afd9832690e

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,30 @@
 Rails.application.routes.draw do
+  get 'genres/index'
+
+  get 'genres/create'
+
+  get 'genres/update'
+
+  get 'genres/show'
+
+  get 'songs/index'
+
+  get 'songs/create'
+
+  get 'songs/update'
+
+  get 'songs/show'
+
+  get 'artists/index'
+
+  get 'artists/create'
+
+  get 'artists/update'
+
+  get 'artists/show'
+
+  resources :genres
+  resources :artists
+  resources :songs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
